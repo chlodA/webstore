96371bd8759bb25fa89e66c696c8bb26c8289646
Chlod Agnieszka <agnieszka.chlod@edu.fh-joanneum.at>
2023-04-25 21:10:35 +0200

    addig some exceptions handling on product

--- src/main/java/com/example/SpringApp/exceptions/NoProductsFoundException.java
+++ src/main/java/com/example/SpringApp/exceptions/NoProductsFoundException.java
@@ -2,7 +2,7 @@ package com.example.SpringApp.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No products found under this category")
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No products found")
 public class NoProductsFoundException extends RuntimeException{
         private static final long serialVersionUID =
         3935230281455340039L;
